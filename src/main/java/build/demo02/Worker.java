be4a6da0ebddc799041eaba510b4e686920ece7a
liupengfeng <1198150498@qq.com>
2020-07-22 23:35:25 +0800

    建造者模式

---
+++ src/main/java/build/demo02/Worker.java
@@ -0,0 +1,42 @@
+package src.main.java.build.demo02;
+
+public class Worker extends Builder{
+    private Product product;
+
+    public Worker() {
+        product = new Product();
+    }
+
+    @Override
+    Builder buildA(String msg) {
+        product.setBuildA(msg);
+        System.out.println(msg);
+        return this;
+    }
+
+    @Override
+    Builder buildB(String msg) {
+        product.setBuildB(msg);
+        System.out.println(msg);
+        return this;
+    }
+
+    @Override
+    Builder buildC(String msg) {
+        product.setBuildC(msg);
+        System.out.println(msg);
+        return this;
+    }
+
+    @Override
+    Builder buildD(String msg) {
+        product.setBuildD(msg);
+        System.out.println(msg);
+        return this;
+    }
+
+    @Override
+    Product getProduct() {
+        return product;
+    }
+}
